cc2e73da500ad5962e49feb18c45beedb7dfcdc2
Antoine <antoine.elmallah@gmail.com>
2024-09-22 12:12:06 -0300

    Adjust in test

--- src/test/java/com/example/demo/mapper/ProductMapperTest.java
+++ src/test/java/com/example/demo/mapper/ProductMapperTest.java
@@ -2,17 +2,23 @@ package com.example.demo.mapper;
 
 import com.example.demo.pojo.Product;
 import com.example.demo.dto.ProductDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mapstruct.factory.Mappers;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ProductMapperTest {
 
+    @Spy
     private ProductMapper mapper = Mappers.getMapper(ProductMapper.class);
 
-    //@Test
+    @Test
     void testMapping() {
         var product = Product.builder()
                 .id(1L)
@@ -25,7 +31,7 @@ class ProductMapperTest {
         assertInstanceOf(ProductDTO.class, productDTO);
 
         assertEquals("PRODUTO 1", productDTO.getName());
-        assertEquals(123.4, productDTO.getValue());
+        assertEquals(new BigDecimal("123.4"), productDTO.getValue());
     }
 
 }
